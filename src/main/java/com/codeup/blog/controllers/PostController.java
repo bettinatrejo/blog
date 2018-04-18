c0d1a742537113a3e2f31c96727a982dfc2e20c2
Bettina Trejo <bettina.trejo12@gmail.com>
2018-04-18 16:49:00 -0500

    methods for creating and editing added

--- src/main/java/com/codeup/blog/controllers/PostController.java
+++ src/main/java/com/codeup/blog/controllers/PostController.java
@@ -1,33 +1,67 @@
 package com.codeup.blog.controllers;
 
+
+import com.codeup.blog.Services.PostService;
+import com.codeup.blog.models.Post;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class PostController {
 
-    @GetMapping(path = "/posts")
-    @ResponseBody
-    public String indexPost() {
-        return "posts index page";
+    PostService pSvc;
+
+    public PostController(PostService pSvc) {
+        this.pSvc = pSvc;
     }
 
-    @GetMapping(path = "/posts/{id}")
-    @ResponseBody
-    public String individualPost(@PathVariable long id) {
-        return "view an individual post" + id;
+
+
+
+    @GetMapping("/posts")
+    public String index(Model model) {
+        model.addAttribute("posts", pSvc.findAll());
+        return "/posts/index";
     }
 
-    @GetMapping(path = "/posts/create")
-    @ResponseBody
-    public String createPost() {
-        return "view the form for creating a post";
+    @GetMapping("/posts/{id}")
+    public String show(@PathVariable long id, Model model) {
+        Post post = new Post("Test Post", "This is a test description.");
+        model.addAttribute("post", pSvc.findPost(id));
+        return "/posts/show";
     }
 
-    @PostMapping(path = "/posts/create")
-    @ResponseBody
-    public String newPost() {
-        return "create a new post";
+    @GetMapping("/posts/create")
+    public String create(Model viewModel) {
+        viewModel.addAttribute("newPost", new Post());
+        return "/posts/create";
     }
+
+
+    @PostMapping("/posts/create")
+    public String insert(@ModelAttribute Post newPost) {
+        pSvc.save(newPost);
+        return "redirect:/posts";
+    }
+
+    @GetMapping("/posts/{id}/edit")
+    public String edit(@PathVariable long id, Model viewModel) {
+        viewModel.addAttribute("post", pSvc.findPost(id));
+        return"/posts/{id}/edit";
+    }
+
+    @PostMapping("/ads/edit")
+    public String handleEdit(@ModelAttribute Post post){
+        System.out.println("post = " + post.getId());
+        return"redirect:/posts";
+    }
+
+
+
+
 
 }
