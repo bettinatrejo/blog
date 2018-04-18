cb20467d9d5a03afc8778f4635912c16a4ba42b1
Bettina Trejo <bettina.trejo12@gmail.com>
2018-04-18 16:48:25 -0500

    controller for welcoming users

--- src/main/java/com/codeup/blog/controllers/HomeController.java
+++ src/main/java/com/codeup/blog/controllers/HomeController.java
@@ -1,15 +1,48 @@
 package com.codeup.blog.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class HomeController {
-    @GetMapping("/")
-    @ResponseBody
-    public String hello() {
-        return "This is the landing page!";
+//    @GetMapping("/")
+//    @ResponseBody
+//    public String hello() {
+//        return "This is the landing page!";
+//    }
+
+
+
+    @GetMapping("/home")
+    public String welcome() {
+        return "home";
     }
+
+    @GetMapping("/home/{name}")
+    public String welcomeUser(@PathVariable String name, Model model) {
+        model.addAttribute("name", name);
+        return "home";
+    }
+
+    @GetMapping("/home/users")
+    public String welcomeUsers(@PathVariable String name, Model model) {
+
+        List<String> users = new ArrayList<>();
+        users.add("John");
+        users.add("Jack");
+
+        model.addAttribute("users", name);
+        return "home";
+    }
+
+
+
+
 
 }
