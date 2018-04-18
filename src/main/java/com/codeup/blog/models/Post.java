72fddaa1a6edfe50cda3d88c9d3b9739aa41a867
Bettina Trejo <bettina.trejo12@gmail.com>
2018-04-18 16:47:30 -0500

    constructor, getters and setters added for post

---
+++ src/main/java/com/codeup/blog/models/Post.java
@@ -0,0 +1,44 @@
+package com.codeup.blog.models;
+
+public class Post {
+
+    private String title;
+    private String body;
+    private long id;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+
+    public Post(String title, String body) {
+        this.title = title;
+        this.body = body;
+    }
+
+    public Post() {
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+
+
+}
