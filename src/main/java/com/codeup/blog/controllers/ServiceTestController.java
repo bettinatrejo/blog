07901411c8b13100db262f0166103ff13f5d57c6
Bettina Trejo <bettina.trejo12@gmail.com>
2018-04-18 16:47:59 -0500

    test, added returnFirstLetter method

---
+++ src/main/java/com/codeup/blog/controllers/ServiceTestController.java
@@ -0,0 +1,24 @@
+package com.codeup.blog.controllers;
+
+import com.codeup.blog.Services.StringTransformSvc;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class ServiceTestController {
+
+    StringTransformSvc stSvc;
+
+    public ServiceTestController(StringTransformSvc stSvc) {
+        this.stSvc = stSvc;
+    }
+
+    @GetMapping("/word/{word}")
+    @ResponseBody
+    public String returnFirstLetter(@PathVariable String word) {
+        String firstLetter= stSvc.getFirstLetter(word);
+        return "The first letter is: "+  firstLetter;
+    }
+}
